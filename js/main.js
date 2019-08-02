ba5358ce499fb1218d00ac306330f9b4e2d7bfee
adamelsbury <adam.john.elsbury@gmail.com>
2019-08-02 17:21:04 +0100

    Modal contents added

--- js/main.js
+++ js/main.js
@@ -31,3 +31,120 @@ function showModalOne() {
     x.style.display = 'none';
   }
 }
+
+function closeModalOne() {
+  var x = document.getElementById('modal-container-1');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+function showModalTwo() {
+  var x = document.getElementById('modal-container-2');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeModalTwo() {
+  var x = document.getElementById('modal-container-2');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+function showModalThree() {
+  var x = document.getElementById('modal-container-3');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeModalThree() {
+  var x = document.getElementById('modal-container-3');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+function showModalFour() {
+  var x = document.getElementById('modal-container-4');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeModalFour() {
+  var x = document.getElementById('modal-container-4');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+function showModalFive() {
+  var x = document.getElementById('modal-container-5');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeModalFive() {
+  var x = document.getElementById('modal-container-5');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+function showModalSix() {
+  var x = document.getElementById('modal-container-6');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeModalSix() {
+  var x = document.getElementById('modal-container-6');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+function showModalSeven() {
+  var x = document.getElementById('modal-container-7');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function closeModalSeven() {
+  var x = document.getElementById('modal-container-7');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
